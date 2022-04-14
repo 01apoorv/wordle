f35a74ca3c03a6814b75ea253e5af26fd65dedd1
Apoorv Chauhan <01apoorv@gmail.com>
2022-04-14 12:40:57 -0400

    made keyboard cool

--- src/App.js
+++ src/App.js
@@ -21,6 +21,14 @@ const App = () => {
   const message2 = "Not enough letters!"
   const message3 = "Yes Meghan, this was my way of asking you on a date :)"
   const message4 = "The word was \"DATE?\" and it was my way of asking you out :/"
+  const [buttArr, setButtArr] = useState(() => {
+    let tmp = []
+    tmp[63] = "secondary"
+    for (let i = 65; i <= 90; i++) {
+      tmp[i] = "secondary"
+    }
+    return tmp
+  })
   const [currMsg, setCurrMsg] = useState("")
   const [gameOver, setGameOver] = useState(false)
   const [first, setFirst] = useState(true)
@@ -41,11 +49,14 @@ const App = () => {
     const checkGuess = () => {
       if (guess.length === 5 && guesses.has(guess)) {
         let tmp = [...status]
+        let tmp2 = [...buttArr]
+        console.log(tmp2)
         let check = ans.split("")
         let gCur = guess.split("")
         for (let i = 0; i < 5; i++) {
           if (gCur[i] === check[i]) {
             tmp[counter][i] = colors[2]
+            tmp2[gCur[i].codePointAt(0)] = "success"
             gCur[i] = ''
             check[i] = ''
           }
@@ -53,6 +64,7 @@ const App = () => {
         if (guess === ans) {
           setCurrMsg(message3)
           setStatus(tmp)
+          setButtArr(tmp2)
           setGameOver(true)
           return;
         }
@@ -60,6 +72,7 @@ const App = () => {
           for (let i = 0; i < 5; i++) {
             if (gCur[i].length > 0 && check.includes(gCur[i])) {
               tmp[counter][i] = colors[3]
+              tmp2[gCur[i].codePointAt(0)] = "warning"
               check[check.indexOf(gCur[i])] = ''
               gCur[i] = ''
             }
@@ -67,10 +80,13 @@ const App = () => {
           for (let i = 0; i < 5; i++) {
             if (gCur[i].length > 0)
               tmp[counter][i] = colors[1]
+              if (!(tmp2[gCur[i].codePointAt(0)] === "success" || tmp2[gCur[i].codePointAt(0)] === "warning"))
+                tmp2[gCur[i].codePointAt(0)] = "dark"
           }
         }
         setGuess("")
         setStatus(tmp)
+        setButtArr(tmp2)
         if (counter === 5) {
           setCurrMsg(message4)
           setGameOver(true)
@@ -157,31 +173,31 @@ const App = () => {
     <div id="row3">
       <Row className="justify-content-center">
         <Col>
-          <Button onClick={() => update("Q")} id="buttonStyle" variant="secondary" size="sm">Q</Button>
-          <Button onClick={() => update("W")} id="buttonStyle" variant="secondary" size="sm">W</Button>
-          <Button onClick={() => update("E")} id="buttonStyle" variant="secondary" size="sm">E</Button>
-          <Button onClick={() => update("R")} id="buttonStyle" variant="secondary" size="sm">R</Button>
-          <Button onClick={() => update("T")} id="buttonStyle" variant="secondary" size="sm">T</Button>
-          <Button onClick={() => update("Y")} id="buttonStyle" variant="secondary" size="sm">Y</Button>
-          <Button onClick={() => update("U")} id="buttonStyle" variant="secondary" size="sm">U</Button>
-          <Button onClick={() => update("I")} id="buttonStyle" variant="secondary" size="sm">I</Button>
-          <Button onClick={() => update("O")} id="buttonStyle" variant="secondary" size="sm">O</Button>
-          <Button onClick={() => update("P")} id="buttonStyle" variant="secondary" size="sm">P</Button>
+          <Button onClick={() => update("Q")} id="buttonStyle" variant={buttArr['Q'.codePointAt(0)]} size="sm">Q</Button>
+          <Button onClick={() => update("W")} id="buttonStyle" variant={buttArr['W'.codePointAt(0)]} size="sm">W</Button>
+          <Button onClick={() => update("E")} id="buttonStyle" variant={buttArr['E'.codePointAt(0)]} size="sm">E</Button>
+          <Button onClick={() => update("R")} id="buttonStyle" variant={buttArr['R'.codePointAt(0)]} size="sm">R</Button>
+          <Button onClick={() => update("T")} id="buttonStyle" variant={buttArr['T'.codePointAt(0)]} size="sm">T</Button>
+          <Button onClick={() => update("Y")} id="buttonStyle" variant={buttArr['Y'.codePointAt(0)]} size="sm">Y</Button>
+          <Button onClick={() => update("U")} id="buttonStyle" variant={buttArr['U'.codePointAt(0)]} size="sm">U</Button>
+          <Button onClick={() => update("I")} id="buttonStyle" variant={buttArr['I'.codePointAt(0)]} size="sm">I</Button>
+          <Button onClick={() => update("O")} id="buttonStyle" variant={buttArr['O'.codePointAt(0)]} size="sm">O</Button>
+          <Button onClick={() => update("P")} id="buttonStyle" variant={buttArr['P'.codePointAt(0)]} size="sm">P</Button>
         </Col>
       </Row>
     </div>
     <div id="row2">
       <Row className="justify-content-center">
         <Col>
-          <Button onClick={() => update("A")} id="buttonStyle" variant="secondary" size="sm">A</Button>
-          <Button onClick={() => update("S")} id="buttonStyle" variant="secondary" size="sm">S</Button>
-          <Button onClick={() => update("D")} id="buttonStyle" variant="secondary" size="sm">D</Button>
-          <Button onClick={() => update("F")} id="buttonStyle" variant="secondary" size="sm">F</Button>
-          <Button onClick={() => update("G")} id="buttonStyle" variant="secondary" size="sm">G</Button>
-          <Button onClick={() => update("H")} id="buttonStyle" variant="secondary" size="sm">H</Button>
-          <Button onClick={() => update("J")} id="buttonStyle" variant="secondary" size="sm">J</Button>
-          <Button onClick={() => update("K")} id="buttonStyle" variant="secondary" size="sm">K</Button>
-          <Button onClick={() => update("L")} id="buttonStyle" variant="secondary" size="sm">L</Button>
+          <Button onClick={() => update("A")} id="buttonStyle" variant={buttArr['A'.codePointAt(0)]} size="sm">A</Button>
+          <Button onClick={() => update("S")} id="buttonStyle" variant={buttArr['S'.codePointAt(0)]} size="sm">S</Button>
+          <Button onClick={() => update("D")} id="buttonStyle" variant={buttArr['D'.codePointAt(0)]} size="sm">D</Button>
+          <Button onClick={() => update("F")} id="buttonStyle" variant={buttArr['F'.codePointAt(0)]} size="sm">F</Button>
+          <Button onClick={() => update("G")} id="buttonStyle" variant={buttArr['G'.codePointAt(0)]} size="sm">G</Button>
+          <Button onClick={() => update("H")} id="buttonStyle" variant={buttArr['H'.codePointAt(0)]} size="sm">H</Button>
+          <Button onClick={() => update("J")} id="buttonStyle" variant={buttArr['J'.codePointAt(0)]} size="sm">J</Button>
+          <Button onClick={() => update("K")} id="buttonStyle" variant={buttArr['K'.codePointAt(0)]} size="sm">K</Button>
+          <Button onClick={() => update("L")} id="buttonStyle" variant={buttArr['L'.codePointAt(0)]} size="sm">L</Button>
         </Col>
       </Row>
     </div>
@@ -189,14 +205,14 @@ const App = () => {
       <Row className="justify-content-center">
         <Col>
           <Button onClick={() => setFlag(!flag)} id="buttonStyle" variant="secondary" size="sm">ENTER</Button>
-          <Button onClick={() => update("Z")} id="buttonStyle" variant="secondary" size="sm">Z</Button>
-          <Button onClick={() => update("X")} id="buttonStyle" variant="secondary" size="sm">X</Button>
-          <Button onClick={() => update("C")} id="buttonStyle" variant="secondary" size="sm">C</Button>
-          <Button onClick={() => update("V")} id="buttonStyle" variant="secondary" size="sm">V</Button>
-          <Button onClick={() => update("B")} id="buttonStyle" variant="secondary" size="sm">B</Button>
-          <Button onClick={() => update("N")} id="buttonStyle" variant="secondary" size="sm">N</Button>
-          <Button onClick={() => update("M")} id="buttonStyle" variant="secondary" size="sm">M</Button>
-          <Button onClick={() => update("?")} id="buttonStyle" variant="secondary" size="sm">?</Button>
+          <Button onClick={() => update("Z")} id="buttonStyle" variant={buttArr['Z'.codePointAt(0)]} size="sm">Z</Button>
+          <Button onClick={() => update("X")} id="buttonStyle" variant={buttArr['X'.codePointAt(0)]} size="sm">X</Button>
+          <Button onClick={() => update("C")} id="buttonStyle" variant={buttArr['C'.codePointAt(0)]} size="sm">C</Button>
+          <Button onClick={() => update("V")} id="buttonStyle" variant={buttArr['V'.codePointAt(0)]} size="sm">V</Button>
+          <Button onClick={() => update("B")} id="buttonStyle" variant={buttArr['B'.codePointAt(0)]} size="sm">B</Button>
+          <Button onClick={() => update("N")} id="buttonStyle" variant={buttArr['N'.codePointAt(0)]} size="sm">N</Button>
+          <Button onClick={() => update("M")} id="buttonStyle" variant={buttArr['M'.codePointAt(0)]} size="sm">M</Button>
+          <Button onClick={() => update("?")} id="buttonStyle" variant={buttArr['?'.codePointAt(0)]} size="sm">?</Button>
           <Button onClick={() => setGuess(guess.slice(0, -1))} id="buttonStyle" variant="secondary" size="sm"><FontAwesomeIcon icon={faDeleteLeft} /></Button>
         </Col>
       </Row>
